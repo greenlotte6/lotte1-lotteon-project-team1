1d82f2291e2cab3328ffb048e28857ed8fd66f76
id3ntity99 <jas06113@gmail.com>
2025-04-15 18:06:26 +0900

    Mapping urls to controllers

--- src/main/java/com/example/lotteon/LotteonApplication.java
+++ src/main/java/com/example/lotteon/LotteonApplication.java
@@ -2,12 +2,14 @@ package com.example.lotteon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//TODO: Delete exclude value below
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LotteonApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LotteonApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(LotteonApplication.class, args);
+  }
 
 }
