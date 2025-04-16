ee080a1e3021be9ced0b3e1fe5e1ae8629648de4
id3ntity99 <jas06113@gmail.com>
2025-04-16 17:45:11 +0900

    Removed useless annotation value

--- src/main/java/com/example/lotteon/LotteonApplication.java
+++ src/main/java/com/example/lotteon/LotteonApplication.java
@@ -2,10 +2,8 @@ package com.example.lotteon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//TODO: Delete exclude value below
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class LotteonApplication {
 
   public static void main(String[] args) {
