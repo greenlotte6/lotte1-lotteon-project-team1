52f9cbb2ac0afc771ffe0ef78309beb18f29a1bb
id3ntity99 <jas06113@gmail.com>
2025-04-23 18:25:38 +0900

    Refactored

--- src/main/resources/static/js/admin/config/basic.js
+++ src/main/resources/static/js/admin/config/basic.js
@@ -1,4 +1,35 @@
 $(() => {
+  function beforeRequest(xhr, url) {
+    console.log(`Initiating request to ${url}`)
+  }
+
+  function onRequestSuccess(res) {
+    alert("수정 완료")
+  }
+
+  function onRequestError(err) {
+    alert(err);
+  }
+
+  function request(url, json) {
+    $.ajax({
+      type: "PUT",
+      url: url,
+      dataType: "json",
+      contentType: "application/json;utf-8",
+      data: JSON.stringify(json),
+      beforeSend: function (xhr) {
+        beforeRequest(xhr, url)
+      },
+      success: function (res) {
+        onRequestSuccess(res);
+      },
+      error: function (err) {
+        onRequestError(err)
+      }
+    })
+  }
+
   // 모든 form 데이터 자동 전송 끄기
   $(".post-form").submit((event) => {
     event.preventDefault();
@@ -9,32 +40,16 @@ $(() => {
     const parent = event.target.parentNode.parentElement;
     const form = parent.querySelector(".post-form")
     const inputs = parent.querySelectorAll("input[type=text]")
-    let jsonStr = {}
+    let json = {}
 
     for (let input of inputs) {
-      jsonStr[input.name] = input.value;
+      json[input.name] = input.value;
     }
-
-    console.log(jsonStr)
 
     const url = form.action + `?key=${key}`;
 
-    $.ajax({
-      type: "PUT",
-      url: url,
-      dataType: "json",
-      contentType: "application/json;utf-8",
-      data: JSON.stringify(jsonStr),
-      beforeSend: function (xhr) {
-        console.log(`Initiating request to ${url}`)
-      },
-      success: function (res) {
-        console.log(res);
-      },
-      error: function (err) {
-        console.log(err)
-      }
-    })
+    request(url, json);
+
   });
 
   $(".post-submit-btn.multipart").click((event) => {
