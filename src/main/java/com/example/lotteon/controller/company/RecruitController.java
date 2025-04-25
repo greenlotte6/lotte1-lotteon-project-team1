0a7412651cbb504f8fc93bdf09934e4948af339b
soljiju <soljiju@gmail.com>
2025-04-25 10:51:03 +0900

    채용 entity 생성

--- src/main/java/com/example/lotteon/controller/company/RecruitController.java
+++ src/main/java/com/example/lotteon/controller/company/RecruitController.java
@@ -1,11 +1,13 @@
 package com.example.lotteon.controller.company;
 
+
 import com.example.lotteon.repository.recruit.RecruitRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequiredArgsConstructor
