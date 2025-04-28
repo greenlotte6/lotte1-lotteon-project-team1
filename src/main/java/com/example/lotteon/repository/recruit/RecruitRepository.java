c1b92c474cabcf72cdde35ad29efd929daf5174f
soljiju <soljiju@gmail.com>
2025-04-28 17:40:49 +0900

    수정

--- src/main/java/com/example/lotteon/repository/recruit/RecruitRepository.java
+++ src/main/java/com/example/lotteon/repository/recruit/RecruitRepository.java
@@ -1,13 +1,13 @@
 package com.example.lotteon.repository.recruit;
 
 import com.example.lotteon.entity.recruit.Recruit;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface RecruitRepository extends JpaRepository<Recruit, Long> {
-    List<Recruit> findByStatus(String status);
-    // 필요한 추가 메서드가 있으면 여기 작성
+    @EntityGraph(attributePaths = {"dept", "career", "hireType"})
+    List<Recruit> findAll();
+
 }
