061ab9b8063dc0bcbb2e1e4e84ce5deb9b6d3464
id3ntity99 <jas06113@gmail.com>
2025-04-28 15:29:34 +0900

    Added dto for point and member

--- src/main/java/com/example/lotteon/dto/user/MemberDTO.java
+++ src/main/java/com/example/lotteon/dto/user/MemberDTO.java
@@ -1,5 +1,6 @@
 package com.example.lotteon.dto.user;
 
+import com.example.lotteon.dto.point.PointDTO;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +16,7 @@ public class MemberDTO {
   private static final String[] GENDERS = {"m", "f"};
 
   private UserCompositeKeyDTO userCompositeKey;
+  private PointDTO point;
   private String name;
   private String gender;
   private LocalDate recentLoginDate;
