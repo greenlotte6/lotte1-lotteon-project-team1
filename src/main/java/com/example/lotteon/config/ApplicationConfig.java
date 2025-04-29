f0e2ea1c7b64185a54433b0db839dd09494c2d87
id3ntity99 <jas06113@gmail.com>
2025-04-30 00:23:19 +0900

    에러 발생시키는 코드 수정

--- src/main/java/com/example/lotteon/config/ApplicationConfig.java
+++ src/main/java/com/example/lotteon/config/ApplicationConfig.java
@@ -4,11 +4,25 @@ import com.example.lotteon.interceptor.GlobalHitCounter;
 import com.example.lotteon.redis.repository.GlobalHitRepository;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
+
+  @Bean
+  public ModelMapper modelMapper() {
+
+    ModelMapper modelMapper = new ModelMapper();
+    modelMapper.getConfiguration()
+        .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+        .setMatchingStrategy(MatchingStrategies.STRICT)
+        .setFieldMatchingEnabled(true);
+
+    return modelMapper;
+  }
 
   @Bean
   public GlobalHitCounter globalHitCounter(GlobalHitRepository repository) {
--- src/main/java/com/example/lotteon/repository/recruit/RecruitRepository.java
+++ src/main/java/com/example/lotteon/repository/recruit/RecruitRepository.java
@@ -1,13 +1,14 @@
 package com.example.lotteon.repository.recruit;
 
 import com.example.lotteon.entity.recruit.Recruit;
+import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface RecruitRepository extends JpaRepository<Recruit, Long> {
-    @EntityGraph(attributePaths = {"dept", "career", "hireType"})
-    List<Recruit> findAll();
 
+  @EntityGraph(attributePaths = {"dept", "career", "hireType"})
+  List<Recruit> findAll();
+
+  List<Recruit> findByStatus(String status);
 }
