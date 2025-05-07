4a58b93029f25ba70f7939a119091ed520f389ca
id3ntity99 <jas06113@gmail.com>
2025-05-07 17:50:04 +0900

    HTML name 속성 변경 및 컨트롤러 시그니쳐 변경

--- src/main/java/com/example/lotteon/controller/admin/order/DeliveryController.java
+++ src/main/java/com/example/lotteon/controller/admin/order/DeliveryController.java
@@ -1,12 +1,10 @@
 package com.example.lotteon.controller.admin.order;
 
 import com.example.lotteon.dto.order.DeliveryDTO;
-import com.example.lotteon.dto.order.OrderDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,12 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DeliveryController {
 
   @PostMapping("register")
-  public String register(OrderDTO order, @RequestParam("deliveryNumber") String deliveryNumber) {
-    //TODO 1. Update order's status to 3(prepare_status)
+  public String register(DeliveryDTO delivery) {
+    //TODO 1. Update order's status to 3(prepare_delivery)
     //TODO 2. Build DeliveryDTO with order and deliveryNumber
     //TODO 3. Insert into `lotteon`.`delivery`
-    DeliveryDTO delivery = DeliveryDTO.builder()
-        .build();
     return "redirect:/admin/order/list";
   }
 }
