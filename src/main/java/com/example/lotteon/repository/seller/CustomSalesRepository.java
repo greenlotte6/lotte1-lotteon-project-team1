ced0995f7cb12c930466d75bb51d1075ee07ead9
id3ntity99 <jas06113@gmail.com>
2025-05-09 10:44:05 +0900

    Implemented SELECT query

--- src/main/java/com/example/lotteon/repository/seller/CustomSalesRepository.java
+++ src/main/java/com/example/lotteon/repository/seller/CustomSalesRepository.java
@@ -1,11 +1,13 @@
 package com.example.lotteon.repository.seller;
 
-import com.example.lotteon.entity.seller.Sales;
+import com.querydsl.core.Tuple;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomSalesRepository {
 
-  List<Sales> findAll();
+  List<Tuple> findAllAsTuples();
+
+  List<Tuple> findAllAsTuples(String sellerId);
 }
--- src/main/java/com/example/lotteon/repository/seller/CustomSalesRepositoryImpl.java
+++ src/main/java/com/example/lotteon/repository/seller/CustomSalesRepositoryImpl.java
@@ -2,11 +2,13 @@ package com.example.lotteon.repository.seller;
 
 import com.example.lotteon.entity.order.QOrder;
 import com.example.lotteon.entity.order.QOrderItem;
+import com.example.lotteon.entity.order.QOrderStatus;
 import com.example.lotteon.entity.product.QProduct;
 import com.example.lotteon.entity.seller.QSales;
 import com.example.lotteon.entity.seller.QSeller;
-import com.example.lotteon.entity.seller.Sales;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,26 +22,79 @@ public class CustomSalesRepositoryImpl implements CustomSalesRepository {
   private final QSales sales = QSales.sales;
   private final QSeller seller = QSeller.seller;
   private final QOrder order = QOrder.order;
+  private final QOrderStatus orderStat = QOrderStatus.orderStatus;
   private final QOrderItem orderItem = QOrderItem.orderItem;
   private final QProduct product = QProduct.product;
 
+  private NumberExpression<Integer> confirmedTotalPriceQuery() {
+    return new CaseBuilder().when(
+            order.status.id.eq(6))
+        .then(product.price
+            .subtract(product.price.multiply(product.discountRate.divide(100)))
+            .multiply(orderItem.amount))
+        .otherwise(0);
+  }
+
+  private NumberExpression<Integer> countByStatusQuery(int statusId) {
+    return new CaseBuilder().when(
+            order.status.id.eq(statusId))
+        .then(1)
+        .otherwise(0);
+  }
+
   @Override
-  public List<Sales> findAll() {
-    List<Tuple> tuples = query.select(seller.sellerId.businessNumber,
+  public List<Tuple> findAllAsTuples() {
+    NumberExpression<Integer> confirmedPriceTotalQuery = confirmedTotalPriceQuery();
+
+    return query.select(sales,
+            order.orderNumber.countDistinct(),
             product.price
                 .subtract(product.price.multiply(product.discountRate.divide(100)))
                 .multiply(orderItem.amount)
-                .sum()
+                .sum(),
+            confirmedPriceTotalQuery.sum(),
+            countByStatusQuery(2),
+            countByStatusQuery(4),
+            countByStatusQuery(5),
+            countByStatusQuery(6)
         )
         .from(sales)
         .join(sales.seller, seller)
-        .join(sales.order, order)
+        .join(order).on(sales.order.orderNumber.eq(order.orderNumber))
+        .join(orderStat).on(order.status.id.eq(orderStat.id))
         .join(orderItem).on(orderItem.order.orderNumber.eq(sales.order.orderNumber))
         .join(product).on(orderItem.product.id.eq(product.id)
             .and(product.seller.sellerId.businessNumber.eq(sales.seller.sellerId.businessNumber)))
         .groupBy(sales.seller.sellerId.businessNumber)
         .fetch();
-    //TODO: Currently working at here
-    return List.of();
+
+  }
+
+  @Override
+  public List<Tuple> findAllAsTuples(String sellerId) {
+    NumberExpression<Integer> confirmedPriceTotalQuery = confirmedTotalPriceQuery();
+
+    return query.select(sales,
+            order.orderNumber.countDistinct(),
+            product.price
+                .subtract(product.price.multiply(product.discountRate.divide(100)))
+                .multiply(orderItem.amount)
+                .sum(),
+            confirmedPriceTotalQuery.sum(),
+            countByStatusQuery(2),
+            countByStatusQuery(4),
+            countByStatusQuery(5),
+            countByStatusQuery(6)
+        )
+        .from(sales)
+        .join(sales.seller, seller)
+        .join(order).on(sales.order.orderNumber.eq(order.orderNumber))
+        .join(orderStat).on(order.status.id.eq(orderStat.id))
+        .join(orderItem).on(orderItem.order.orderNumber.eq(sales.order.orderNumber))
+        .join(product).on(orderItem.product.id.eq(product.id)
+            .and(product.seller.sellerId.businessNumber.eq(sales.seller.sellerId.businessNumber)))
+        .where(sales.seller.sellerId.user.id.eq(sellerId))
+        .groupBy(sales.seller.sellerId.businessNumber)
+        .fetch();
   }
 }
