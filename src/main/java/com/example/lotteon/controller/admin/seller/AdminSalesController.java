89f9bb0709cd0d5be6bd8a167046a73061c2a0f0
id3ntity99 <jas06113@gmail.com>
2025-05-09 10:45:04 +0900

    매출현황 페이지 목록 기능 구현 완료

--- src/main/java/com/example/lotteon/controller/admin/seller/AdminSalesController.java
+++ src/main/java/com/example/lotteon/controller/admin/seller/AdminSalesController.java
@@ -1,10 +1,19 @@
 package com.example.lotteon.controller.admin.seller;
 
+import com.example.lotteon.dto.seller.SalesWrapper;
 import com.example.lotteon.service.seller.SalesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,8 +23,31 @@ public class AdminSalesController {
   private final SalesService service;
 
   @GetMapping("/list")
-  public String list() {
-    service.findAll();
+  public String list(@RequestParam(name = "page", defaultValue = "1") int page,
+      @RequestParam(name = "size", defaultValue = "10") int size,
+      Model model) {
+    Pageable pageable = PageRequest.of(page - 1, size);
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    UserDetails details = (UserDetails) auth.getPrincipal();
+    boolean isSeller = details.getAuthorities().stream()
+        .anyMatch(g -> g.getAuthority().equals("ROLE_SELLER"));
+
+    Page<SalesWrapper> pages = null;
+    String role = null;
+
+    if (isSeller) {
+      String sellerId = details.getUsername();
+      pages = service.list(sellerId, pageable);
+      role = "ROLE_SELLER";
+    } else {
+      pages = service.list(pageable);
+      role = "ROLE_ADMIN";
+    }
+
+    model.addAttribute("currentPage", page);
+    model.addAttribute("pages", pages);
+    model.addAttribute("role", role);
+
     return "/admin/shop/sales";
   }
 }
--- src/main/java/com/example/lotteon/service/seller/SalesService.java
+++ src/main/java/com/example/lotteon/service/seller/SalesService.java
@@ -1,9 +1,15 @@
 package com.example.lotteon.service.seller;
 
-import com.example.lotteon.entity.seller.Sales;
+import com.example.lotteon.dto.seller.SalesWrapper;
 import com.example.lotteon.repository.seller.SalesRepository;
+import com.querydsl.core.Tuple;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +17,29 @@ import org.springframework.stereotype.Service;
 public class SalesService {
 
   private final SalesRepository repo;
+  private final ModelMapper mapper;
 
-  public List<Sales> findAll() {
-    return repo.findAll();
+  private List<SalesWrapper> toList(List<Tuple> tuples) {
+    List<SalesWrapper> wrappers = new ArrayList<>();
+
+    for (Tuple tuple : tuples) {
+      SalesWrapper wrapper = SalesWrapper.builder()
+          .tuple(tuple)
+          .build(mapper);
+      wrappers.add(wrapper);
+    }
+    return wrappers;
+  }
+
+  public Page<SalesWrapper> list(Pageable pageable) {
+    List<Tuple> tuples = repo.findAllAsTuples();
+    List<SalesWrapper> wrappers = toList(tuples);
+    return new PageImpl<>(wrappers, pageable, wrappers.size());
+  }
+
+  public Page<SalesWrapper> list(String currentSellerId, Pageable pageable) {
+    List<Tuple> tuples = repo.findAllAsTuples(currentSellerId);
+    List<SalesWrapper> wrappers = toList(tuples);
+    return new PageImpl<>(wrappers, pageable, wrappers.size());
   }
 }
