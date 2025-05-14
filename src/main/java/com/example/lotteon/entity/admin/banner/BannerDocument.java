515073d80e632a83ca62417b126d8dfe51f05a79
id3ntity99 <jas06113@gmail.com>
2025-05-14 15:50:57 +0900

    Document class for MongoDB

---
+++ src/main/java/com/example/lotteon/entity/admin/banner/BannerDocument.java
@@ -0,0 +1,44 @@
+package com.example.lotteon.entity.admin.banner;
+
+import com.google.gson.annotations.SerializedName;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Data
+@Builder
+@Document(collection = "banner")
+@NoArgsConstructor
+@AllArgsConstructor
+public class BannerDocument {
+
+  @Id
+  @Field("_id")
+  @SerializedName("_id")
+  private String _id;
+
+  @Field("id")
+  @SerializedName("id")
+  private String id;
+
+  @Field("id")
+  @SerializedName("banner_info")
+  private BannerInfo bannerInfo;
+
+  @Field("location")
+  @SerializedName("location")
+  private String location;
+
+  @Field("expiration")
+  @SerializedName("expiration")
+  private LocalDateTime expiration;
+
+  @Field("status")
+  @SerializedName("status")
+  private String status = "inactive";
+}
